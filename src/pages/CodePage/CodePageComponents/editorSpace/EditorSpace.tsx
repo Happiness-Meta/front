79fe5d5c928891d49ccb0a22cae1aa6f2c2e00d5
Matilda0730 <matilda230730@gmail.com>
2024-02-08 17:39:13 +0900

    [FEAT] 채팅 기능 구현 중

--- src/pages/CodePage/CodePageComponents/editorSpace/EditorSpace.tsx
+++ src/pages/CodePage/CodePageComponents/editorSpace/EditorSpace.tsx
@@ -4,7 +4,7 @@ import Editor from "@monaco-editor/react";
 import { Resizable } from "re-resizable";
 import globalStore from "../../../../store/globalStore/globalStore";
 import editorStore from "../../../../store/CodePageStore/editorStore";
-import RightSpace from "./rightSpace/RightSpace";
+import ChatSpace from "./ChatSpace/ChatSpace";
 // import * as Y from "yjs";
 // import { WebrtcProvider } from "y-webrtc";
 // import { MonacoBinding } from "y-monaco";
@@ -15,15 +15,9 @@ function EditorSpace() {
   const { tabs, rightSpace, terminal } = editorStore();
 
   return (
-    <div
-      className={`${sidebar ? styles.sidebarToggle : undefined} ${
-        styles.editorSpace
-      }`}
-    >
+    <div className={`${sidebar ? styles.sidebarToggle : undefined} ${styles.editorSpace}`}>
       <Resizable
-        className={`${rightSpace ? styles.leftWidthFull : undefined} ${
-          styles.leftSpace
-        }`}
+        className={`${rightSpace ? styles.leftWidthFull : undefined} ${styles.leftSpace}`}
         defaultSize={{ width: "50%", height: "100%" }}
         enable={{ top: false, bottom: false, right: true, left: false }}
         handleClasses={{ right: "resizeHandle2" }}
@@ -32,36 +26,24 @@ function EditorSpace() {
           {tabs.map((tab) => (
             <div
               className={styles.tab}
-              style={
-                mode
-                  ? { backgroundColor: "white" }
-                  : { backgroundColor: "#1e1e1e" }
-              }
+              style={mode ? { backgroundColor: "white" } : { backgroundColor: "#1e1e1e" }}
             >
               <button className={styles.tabEach}>
                 <img src="/svg/html.svg" className={styles.tabIcon} />
                 <span className={styles.tabName}>{tab.title}</span>
               </button>
-              <button
-                className={`${styles.tabRemove} material-symbols-outlined`}
-              >
-                close
-              </button>
+              <button className={`${styles.tabRemove} material-symbols-outlined`}>close</button>
             </div>
           ))}
         </div>
         <div
           className={styles.filePathSpace}
-          style={
-            mode ? { backgroundColor: "white" } : { backgroundColor: "#1e1e1e" }
-          }
+          style={mode ? { backgroundColor: "white" } : { backgroundColor: "#1e1e1e" }}
         ></div>
         <div className={styles.editorAndTerminal}>
           <Resizable
             defaultSize={{ width: "100%", height: "calc(100% - 56px)" }}
-            className={`${terminal ? styles.withTerminal : undefined} ${
-              styles.editorWrapper
-            }`}
+            className={`${terminal ? styles.withTerminal : undefined} ${styles.editorWrapper}`}
             enable={{ top: false, bottom: true, right: false, left: false }}
             handleClasses={{ top: "resizeHandle3" }}
           >
@@ -77,16 +59,12 @@ function EditorSpace() {
               }}
             />
           </Resizable>
-          <div
-            className={`${terminal ? styles.terminalOn : undefined} ${
-              styles.terminalSpace
-            }`}
-          >
+          <div className={`${terminal ? styles.terminalOn : undefined} ${styles.terminalSpace}`}>
             <div className={styles.terminal_header}>터미널</div>
           </div>
         </div>
       </Resizable>
-      <RightSpace />
+      <ChatSpace />
     </div>
   );
 }
