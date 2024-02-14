c01ac560a44404fbc6ce4a8f92e9b995e39f8e20
YunseokChoi <hiyunseok347@gmail.com>
2024-02-14 17:18:57 +0900

    FEAT : 로그인 회원가입 기능 react query로 변경

--- src/pages/SignInUpPage/SignInUpComponents/SignIn.tsx
+++ src/pages/SignInUpPage/SignInUpComponents/SignIn.tsx
@@ -5,6 +5,7 @@ import LoginFormDto from "../../../dto/LoginFormDto";
 import axios, { AxiosError } from "axios";
 import { useNavigate } from "react-router-dom";
 import SignInStore from "../../../store/LoginPageStore/SignInStore";
+import { useMutation } from "@tanstack/react-query";
 
 function SignIn() {
   const navigate = useNavigate();
@@ -28,42 +29,84 @@ function SignIn() {
     setSignInPw(e.target.value);
   };
 
-  const loginUser = async () => {
-    if (idInput.current!.value === "")
-      return signInErrorMessageStatus("아이디를 입력해주세요");
-    if (pwInput.current!.value === "")
-      return signInErrorMessageStatus("비밀번호를 입력해주세요");
-    const body: LoginFormDto = {
-      email: signInId,
-      password: signInPw,
-    };
+  const loginUser = useMutation({
+    mutationFn: async () => {
+      if (idInput.current!.value === "")
+        return signInErrorMessageStatus("아이디를 입력해주세요.");
+      if (pwInput.current!.value === "")
+        return signInErrorMessageStatus("비밀번호를 입력해주세요.");
 
-    try {
-      const response = await axios.post(
-        // "http://43.203.92.111//api/sign/login",
-        "http://localhost:8080/api/sign/login",
-        body
-      );
+      const body: LoginFormDto = {
+        email: signInId,
+        password: signInPw,
+      };
 
-      console.log(response.status);
+      try {
+        const response = await axios.post(
+          // "http://43.203.92.111//api/sign/login",
+          "http://localhost:8080/api/sign/login",
+          body
+        );
 
-      signInErrorMessageStatus("");
-      navigate("/dashboard");
+        console.log(response.status);
 
-      console.log(response.data);
-    } catch (error) {
-      if (axios.isAxiosError(error)) {
-        const axiosError = error as AxiosError;
+        signInErrorMessageStatus("");
+        navigate("/dashboard");
 
-        if (axiosError.response) {
-          console.log(error.response?.data);
-          if (error.response?.data.code === 400) {
-            return signInErrorMessageStatus("로그인 정보가 올바르지 않습니다.");
+        console.log(response.data);
+      } catch (error) {
+        if (axios.isAxiosError(error)) {
+          const axiosError = error as AxiosError;
+
+          if (axiosError.response) {
+            console.log(error.response?.data);
+            if (error.response?.data.code === 400) {
+              return signInErrorMessageStatus(
+                "이메일 또는 비밀번호를 잘못 입력하였습니다."
+              );
+            }
           }
         }
       }
-    }
-  };
+    },
+  });
+
+  // const loginUser = async () => {
+  //   if (idInput.current!.value === "")
+  //     return signInErrorMessageStatus("아이디를 입력해주세요");
+  //   if (pwInput.current!.value === "")
+  //     return signInErrorMessageStatus("비밀번호를 입력해주세요");
+  //   const body: LoginFormDto = {
+  //     email: signInId,
+  //     password: signInPw,
+  //   };
+
+  //   try {
+  //     const response = await axios.post(
+  //       // "http://43.203.92.111//api/sign/login",
+  //       "http://localhost:8080/api/sign/login",
+  //       body
+  //     );
+
+  //     console.log(response.status);
+
+  //     signInErrorMessageStatus("");
+  //     navigate("/dashboard");
+
+  //     console.log(response.data);
+  //   } catch (error) {
+  //     if (axios.isAxiosError(error)) {
+  //       const axiosError = error as AxiosError;
+
+  //       if (axiosError.response) {
+  //         console.log(error.response?.data);
+  //         if (error.response?.data.code === 400) {
+  //           return signInErrorMessageStatus("로그인 정보가 올바르지 않습니다.");
+  //         }
+  //       }
+  //     }
+  //   }
+  // };
 
   return (
     <div className={styles.signInSection}>
@@ -117,7 +160,8 @@ function SignIn() {
         <button
           className={styles.signInUpBtn}
           onClick={() => {
-            loginUser();
+            loginUser.mutate();
+            // loginUser();
             signInErrorMessageAniToggle();
           }}
         >
