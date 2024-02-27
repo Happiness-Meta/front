f06ee75fdd99ac83ab99c64a8b11e90415cfe060
YunseokChoi <hiyunseok347@gmail.com>
2024-02-27 17:10:13 +0900

    FIX : 절대경로 되게 고침

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
-import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -19,8 +18,9 @@ export default defineConfig({
     chunkSizeWarningLimit: 1500,
   },
   resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-    },
+    alias: [
+      { find: "@components", replacement: "/src/components" },
+      { find: "@", replacement: "/src" },
+    ],
   },
 });
